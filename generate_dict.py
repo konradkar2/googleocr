c09277a15673127ede407b8e6a0807221f534511
unknown <konradkar2@gmail.com>
2021-05-04 13:36:05 +0200

    removed small characters from labels

--- generate_dict.py
+++ generate_dict.py
@@ -7,14 +7,14 @@ end1 = ord('9') + 1
 start2 = ord('A')
 end2 = ord('Z') +1 
 
-start3 = ord('a')
-end3 = ord('z') + 1
+#start3 = ord('a')
+#end3 = ord('z') + 1
 
 f = open("charlabels.txt", "w")
 for x in range(start1,end1):
     f.write(chr(x) +'\n')
 for x in range(start2,end2):
     f.write(chr(x)+'\n')
-for x in range(start3,end3):
-    f.write(chr(x)+'\n')
+#for x in range(start3,end3):
+#    f.write(chr(x)+'\n')
 f.close()
